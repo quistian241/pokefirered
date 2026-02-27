b208950b1a337ba9d6f97db453d63e26440734b7
quistian241 <shreaktears@gmail.com>
2026-02-26 20:45:37 -0800

    Rebalanced Dragon-type moves

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -2603,7 +2603,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_OUTRAGE] =
     {
         .effect = EFFECT_RAMPAGE,
-        .power = 90,
+        .power = 120,
         .type = TYPE_DRAGON,
         .accuracy = 100,
         .pp = 15,
@@ -2928,7 +2928,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_DRAGON_BREATH] =
     {
         .effect = EFFECT_PARALYZE_HIT,
-        .power = 60,
+        .power = 65,
         .type = TYPE_DRAGON,
         .accuracy = 100,
         .pp = 20,
